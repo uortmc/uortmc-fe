ade78e79e8d9cbe46f5952e7dcc034c5a78a29db
Stefanos Stefanou <stefannstefanou@gmail.com>
2021-02-19 23:43:04 +0000

    [161+162] New About page + Navbar bug fixed

--- src/components/content/about/about.js
+++ src/components/content/about/about.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './about.css'
+import logo from '../../../resources/logo2.png'
 class About extends React.Component{
     constructor(props) {
         super(props);
@@ -12,11 +13,18 @@ class About extends React.Component{
             <div className="card-header">
                 UORTMC About page
             </div>
-            <div className="card-body">
-                <h5 className="card-title">Thyroid Module Classifier(TMC)</h5>
-                <p className="card-text">2020-2021 Bsc thesis project</p>
-                <p className="card-text">Bsc Candidate: Stefanos Stefanou</p>
-                <p className="card-text">Supervisor: Huizhi Liang</p>
+            <div className="row">
+                <div className="col-4">
+                    <img src={logo}  className="tmc_about_logo"/>
+                </div>
+                <div className="col-8 card-body ">
+                    <div className="tmc_about_elements">
+                        <h5 className="card-title">Thyroid Module Classifier(TMC)</h5>
+                        <p className="card-text">2020-2021 Bsc thesis project</p>
+                        <p className="card-text">Bsc Candidate: Stefanos Stefanou</p>
+                        <p className="card-text">Supervisor: Huizhi Liang</p>
+                    </div>
+                </div>
             </div>
             <div className="card-footer text-muted">
                 Version 1.0.0
